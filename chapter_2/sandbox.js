6e1c049f4a605bcf149e9da7f9a5bf9948ba3e86
Akshaya Rajesh <akshayatr1997@gmail.com>
2021-03-24 12:29:41 +0530

    Variables

--- chapter_2/sandbox.js
+++ chapter_2/sandbox.js
@@ -1,1 +1,17 @@
-alert('hello, world!');
+//alert('hello, world!');
+
+/*console.log(1);
+console.log(2);*/
+
+let age = 25; //declaring a new variable and initializing it with a value
+let year = 2021;
+console.log(age,year);
+
+age = 30;
+console.log(age);
+
+const points = 100; //creating a constant variable
+console.log(points);
+
+var score = 75; //older way to create a variable
+console.log(score);
