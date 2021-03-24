2d2cfde6dfdc8fe70b316d2cbf25f60753362306
Akshaya Rajesh <akshayatr1997@gmail.com>
2021-03-24 15:15:50 +0530

    Numbers

---
+++ chapter_4/sandbox.js
@@ -0,0 +1,25 @@
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius, pi);
+
+//math operators : +, -, *, /, **, %
+
+console.log(10/2);
+
+let r1 = radius % 3;
+console.log(r1);
+
+let r2 = pi * radius**2;
+console.log(r2);
+
+let likes = 10;
+likes = likes + 1;
+likes++;
+likes--;
+likes+=10;
+console.log(likes);
+
+//NaN : Not a Number
+console.log(5 / 'hello');
+console.log(5 * 'hello');
